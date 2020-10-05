f76aeaf124f05c516da9ce1874f2339d6905e04b
sam <sam@freighttrust.com>
2020-10-05 12:51:32 -0700

    init truffle ci

--- truffle-config.js
+++ truffle-config.js
@@ -42,6 +42,14 @@ module.exports = {
       network_id: "*",       // Any network (default: none)
       gas: 1000000,          // Gas sent with each transaction (default: ~6700000)
     },
+
+    coverage: {
+      host: 'localhost',
+      network_id: '*',
+      port: 8554, // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01, // <-- Use this low gas price
+    }
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
